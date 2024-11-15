b1076e956f23378dac88687b47ed695545a6bdad
dekkerglen <dekker.glen@gmail.com>
2024-11-15 12:46:03 -0500

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -29,14 +29,6 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     if (!isCommit(evt)) return
 
     const ops = await getOpsByType(evt)
-
-    // This logs the text of every post off the firehose.
-    // Just for fun :)
-    // Delete before actually using
-    for (const post of ops.posts.creates) {
-      console.log(post.record.text)
-    }
-
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
@@ -63,6 +55,8 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
         .execute()
     }
     if (postsToCreate.length > 0) {
+      console.log(`Adding ${postsToCreate.length} posts`)
+
       await this.db
         .insertInto('post')
         .values(postsToCreate)
